e3e21a60a19e12d57d3cdcaf0cd59e954032e8d3
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2022-01-16 05:39:48 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+var HeaderSearch = document.getElementsByClassName('HeaderSearch')[0];
+var OpenMenu = document.getElementById("OpenMenu");
 
+OpenMenu.onclick = function(){
+	HeaderSearch.style.display = "block";
+	OpenMenu.style.display = "none";
+}
+
+Window.onclick = function(event){
+	if(event.target == HeaderSearch){
+    HeaderSearch.style.display = "none";
+	}
+}
